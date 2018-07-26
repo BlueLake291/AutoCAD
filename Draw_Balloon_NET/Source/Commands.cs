90e5854666a9adacb82e61f4cad3229ee0e85efa
BlueLake291 <ducmanhbk.201@gmail.com>
2018-07-26 10:31:40 +0700

    update file Commands.cs

--- Draw_Balloon_NET/Source/Commands.cs
+++ Draw_Balloon_NET/Source/Commands.cs
@@ -527,6 +527,81 @@ namespace Draw_Balloon_net
         }
         #endregion
 
+
+        #region Find interference between solids
+        [CommandMethod("InterferenceSolids")]
+        public void interferenceSolids()
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                BlockTable blkTable = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                if (blkTable == null)
+                {
+                    return;
+                }
+
+                BlockTableRecord blkTableRecord = trans.GetObject(blkTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                if (blkTableRecord == null)
+                {
+                    return;
+                }
+
+                using (Solid3d solid3DBox = new Solid3d())
+                {
+                    // 3D Solid Box.
+                    solid3DBox.CreateBox(5, 7, 10);
+                    solid3DBox.ColorIndex = 7;
+
+                    // Position of the center of solid3DBox at (5, 5, 0)
+                    solid3DBox.TransformBy(Matrix3d.Displacement(new Point3d(5, 5, 0) - Point3d.Origin));
+
+                    //blkTableRecord.AppendEntity(solid3DBox);
+                    //trans.AddNewlyCreatedDBObject(solid3DBox, true);
+
+                    // 3D Solid Cylinder. 
+                    using (Solid3d solid3DCylinder = new Solid3d())
+                    {
+                        solid3DCylinder.CreateFrustum(20, 5, 5, 5);
+                        solid3DCylinder.ColorIndex = 4;
+
+                        //blkTableRecord.AppendEntity(solid3DCylinder);
+                        //trans.AddNewlyCreatedDBObject(solid3DCylinder, true);
+
+                        // Create 3D solid from the interference of the box and cylinder. 
+                        //Solid3d solid3dCopy = solid3DCylinder.Clone() as Solid3d;
+
+                        //if (solid3dCopy.CheckInterference(solid3DBox) == true)
+                        //{
+                        //    solid3dCopy.BooleanOperation(BooleanOperationType.BoolSubtract, solid3DBox.Clone() as Solid3d);
+                        //    solid3dCopy.ColorIndex = 1;
+                        //}
+
+                        //// add solid3dCopy to the block table record. 
+                        //blkTableRecord.AppendEntity(solid3dCopy);
+                        //trans.AddNewlyCreatedDBObject(solid3dCopy, true);
+
+                        Solid3d solid3dCopyCylinder = solid3DCylinder.Clone() as Solid3d;
+
+                        if (solid3dCopyCylinder.CheckInterference(solid3DBox) == true)
+                        {
+                            solid3dCopyCylinder.BooleanOperation(BooleanOperationType.BoolIntersect, solid3DBox);
+                            solid3dCopyCylinder.ColorIndex = 3;
+                        }
+
+                        blkTableRecord.AppendEntity(solid3dCopyCylinder);
+                        trans.AddNewlyCreatedDBObject(solid3dCopyCylinder, true);
+                    }
+                }
+
+                trans.Commit();
+            }
+
+        }
+
+        #endregion
     }
     #endregion
 
