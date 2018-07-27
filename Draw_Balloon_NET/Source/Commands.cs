53d2c363dfda014c596fc45c1a54b90b171814b7
BlueLake291 <ducmanhbk.201@gmail.com>
2018-07-27 10:48:48 +0700

    update file Command.cs

--- Draw_Balloon_NET/Source/Commands.cs
+++ Draw_Balloon_NET/Source/Commands.cs
@@ -226,39 +226,47 @@ namespace Draw_Balloon_net
 
             using (Transaction trans = dwg.TransactionManager.StartTransaction())
             {
-                BlockTable blk = trans.GetObject(dwg.BlockTableId, OpenMode.ForWrite) as BlockTable;
-                if (blk == null)
+                try
                 {
-                    return;
+                    BlockTable blk = trans.GetObject(dwg.BlockTableId, OpenMode.ForWrite) as BlockTable;
+                    if (blk == null)
+                    {
+                        return;
+                    }
+
+                    // make text for circle. 
+                    DBText _textBal = makeText(_circleBal.Center);
+                    _textBal.ColorIndex = setting.IndexColorText;
+                    _textBal.TextString = setting.Text;
+
+                    // make the list of objectId of each elements. 
+                    List<ObjectId> lstObjectId = new List<ObjectId>();
+
+                    // append elements into database. 
+                    BlockTableRecord blkRecord = trans.GetObject(blk[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                    ObjectId objLineId = blkRecord.AppendEntity(_lineBal);
+                    lstObjectId.Add(objLineId);
+
+                    ObjectId objCircleId = blkRecord.AppendEntity(_circleBal);
+                    lstObjectId.Add(objCircleId);
+
+                    ObjectId objTextId = blkRecord.AppendEntity(_textBal);
+                    lstObjectId.Add(objTextId);
+
+                    // save id of each elements. 
+                    ImplementationDatabase.LstObjectId = lstObjectId;
+
+                    trans.AddNewlyCreatedDBObject(_lineBal, true);
+                    trans.AddNewlyCreatedDBObject(_circleBal, true);
+                    trans.AddNewlyCreatedDBObject(_textBal, true);
+
+                    trans.Commit();
                 }
-
-                // make text for circle. 
-                DBText _textBal = makeText(_circleBal.Center);
-                _textBal.ColorIndex = setting.IndexColorText;
-                _textBal.TextString = setting.Text;
-
-                // make the list of objectId of each elements. 
-                List<ObjectId> lstObjectId = new List<ObjectId>();
-
-                // append elements into database. 
-                BlockTableRecord blkRecord = trans.GetObject(blk[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                ObjectId objLineId = blkRecord.AppendEntity(_lineBal);
-                lstObjectId.Add(objLineId);
-
-                ObjectId objCircleId = blkRecord.AppendEntity(_circleBal);
-                lstObjectId.Add(objCircleId);
-
-                ObjectId objTextId = blkRecord.AppendEntity(_textBal);
-                lstObjectId.Add(objTextId);
-
-                // save id of each elements. 
-                ImplementationDatabase.LstObjectId = lstObjectId;
-
-                trans.AddNewlyCreatedDBObject(_lineBal, true);
-                trans.AddNewlyCreatedDBObject(_circleBal, true);
-                trans.AddNewlyCreatedDBObject(_textBal, true);
-
-                trans.Commit();
+                catch (System.Exception)
+                {
+                    trans.Abort();
+                    throw;
+                }                
             }
 
             // get name for this group.
@@ -329,20 +337,28 @@ namespace Draw_Balloon_net
             Database dwg = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
             using (Transaction trans = dwg.TransactionManager.StartTransaction())
             {
-                // use the transaction to open these objects.
-                // the transaction will automatically dispose these objects when done
-                // so we don't have to worry about manually disposing them.
-                BlockTable blk = trans.GetObject(dwg.BlockTableId, OpenMode.ForWrite) as BlockTable;
-                if (blk == null)
+                try
                 {
-                    return;
+                    // use the transaction to open these objects.
+                    // the transaction will automatically dispose these objects when done
+                    // so we don't have to worry about manually disposing them.
+                    BlockTable blk = trans.GetObject(dwg.BlockTableId, OpenMode.ForWrite) as BlockTable;
+                    if (blk == null)
+                    {
+                        return;
+                    }
+
+                    BlockTableRecord blkRecord = trans.GetObject(blk[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                    blkRecord.AppendEntity(circle);
+
+                    trans.AddNewlyCreatedDBObject(circle, true);
+                    trans.Commit();
                 }
-
-                BlockTableRecord blkRecord = trans.GetObject(blk[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                blkRecord.AppendEntity(circle);
-
-                trans.AddNewlyCreatedDBObject(circle, true);
-                trans.Commit();
+                catch (System.Exception)
+                {
+                    trans.Abort();
+                    throw;
+                }                                
             }
         }
         #endregion
@@ -369,64 +385,72 @@ namespace Draw_Balloon_net
             
             using (Transaction trans = db.TransactionManager.StartTransaction())
             {
-                //Select first polyline
-                PromptEntityOptions ptEntityOpt = new PromptEntityOptions("Select firtst line:");
-                PromptEntityResult ptEntityRes = ed.GetEntity(ptEntityOpt);
-                if (ptEntityRes.Status != PromptStatus.OK)
+                try
                 {
-                    return;
-                }
+                    //Select first polyline
+                    PromptEntityOptions ptEntityOpt = new PromptEntityOptions("Select firtst line:");
+                    PromptEntityResult ptEntityRes = ed.GetEntity(ptEntityOpt);
+                    if (ptEntityRes.Status != PromptStatus.OK)
+                    {
+                        return;
+                    }
 
-                //Get the polyline entity
-                ent = (Entity)trans.GetObject(ptEntityRes.ObjectId, OpenMode.ForRead);
-                if (ent is Line)
+                    //Get the polyline entity
+                    ent = (Entity)trans.GetObject(ptEntityRes.ObjectId, OpenMode.ForRead);
+                    if (ent is Line)
+                    {
+                        firstLine = ent as Line;
+                    }
+
+                    //Select 2nd polyline
+                    ptEntityOpt = new PromptEntityOptions("\n Select the second line or the circle:");
+                    ptEntityRes = ed.GetEntity(ptEntityOpt);
+                    if (ptEntityRes.Status != PromptStatus.OK)
+                    {
+                        return;
+                    }
+
+                    ent = (Entity)trans.GetObject(ptEntityRes.ObjectId, OpenMode.ForRead);
+                    if (ent is Line)
+                    {
+                        secondLine = ent as Line;
+                    }
+                    else if (ent is Circle)
+                    {
+                        cir = ent as Circle;
+                        st = State.CircleType;
+                    }
+
+                    Point3dCollection pts3D = new Point3dCollection();
+
+                    //Get the intersection Points.
+                    switch (st)
+                    {
+                        case State.LineType:
+                            firstLine.IntersectWith(secondLine, Intersect.OnBothOperands, pts3D, IntPtr.Zero, IntPtr.Zero);
+                            break;
+
+                        case State.CircleType:
+                            firstLine.IntersectWith(cir, Intersect.OnBothOperands, pts3D, IntPtr.Zero, IntPtr.Zero);
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    foreach (Point3d pt in pts3D)
+                    {
+                        ed.WriteMessage("Point number: " + pt.X + " " + pt.Y + " " + pt.Z);
+                        //Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("\n Intersection Point: " + "\nX = " + pt.X + "\nY = " + pt.Y + "\nZ = " + pt.Z);
+                    }
+
+                    trans.Commit();
+                }
+                catch (System.Exception)
                 {
-                    firstLine = ent as Line;
-                }
-
-                //Select 2nd polyline
-                ptEntityOpt = new PromptEntityOptions("\n Select the second line or the circle:");
-                ptEntityRes = ed.GetEntity(ptEntityOpt);
-                if (ptEntityRes.Status != PromptStatus.OK)
-                {
-                    return;
-                }
-
-                ent = (Entity)trans.GetObject(ptEntityRes.ObjectId, OpenMode.ForRead);
-                if (ent is Line)
-                {
-                    secondLine = ent as Line;                   
-                }
-                else if (ent is Circle)
-                {
-                    cir = ent as Circle;
-                    st = State.CircleType;
-                }
-
-                Point3dCollection pts3D = new Point3dCollection();
-
-                //Get the intersection Points.
-                switch (st)
-                {
-                    case State.LineType:
-                        firstLine.IntersectWith(secondLine, Intersect.OnBothOperands, pts3D, IntPtr.Zero, IntPtr.Zero);
-                        break;
-
-                    case State.CircleType:
-                        firstLine.IntersectWith(cir, Intersect.OnBothOperands, pts3D, IntPtr.Zero, IntPtr.Zero);
-                        break;
-
-                    default:
-                        break;
-                }
-                
-                foreach (Point3d pt in pts3D)
-                {                    
-                    ed.WriteMessage("Point number: " + pt.X + " " + pt.Y + " " + pt.Z);
-                    //Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("\n Intersection Point: " + "\nX = " + pt.X + "\nY = " + pt.Y + "\nZ = " + pt.Z);
-                }
-
-                trans.Commit();
+                    trans.Abort();
+                    throw;
+                }                
             }
         }
         #endregion
@@ -465,30 +489,38 @@ namespace Draw_Balloon_net
 
             using (Transaction trans = dwg.TransactionManager.StartTransaction())
             {
-                BlockTable blk = trans.GetObject(dwg.BlockTableId, OpenMode.ForWrite) as BlockTable;
-                if (blk == null)
+                try
                 {
-                    return;
+                    BlockTable blk = trans.GetObject(dwg.BlockTableId, OpenMode.ForWrite) as BlockTable;
+                    if (blk == null)
+                    {
+                        return;
+                    }
+
+                    Circle circle = new Circle(ptCenter, Vector3d.ZAxis, radius);
+
+                    // make region.
+                    DBObjectCollection dbObjCollec = new DBObjectCollection();
+                    dbObjCollec.Add(circle);
+
+                    DBObjectCollection regionObjCollec = Region.CreateFromCurves(dbObjCollec);
+
+                    Solid3d solid3d = new Solid3d();
+                    solid3d.Extrude((Region)regionObjCollec[0], height, 0.0);
+
+                    // append elements into database. 
+                    BlockTableRecord blkRecord = trans.GetObject(blk[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                    blkRecord.AppendEntity(solid3d);
+
+                    trans.AddNewlyCreatedDBObject(solid3d, true);
+
+                    trans.Commit();
                 }
-
-                Circle circle = new Circle(ptCenter, Vector3d.ZAxis, radius);
-
-                // make region.
-                DBObjectCollection dbObjCollec = new DBObjectCollection();
-                dbObjCollec.Add(circle);
-
-                DBObjectCollection regionObjCollec = Region.CreateFromCurves(dbObjCollec);
-                                         
-                Solid3d solid3d = new Solid3d();
-                solid3d.Extrude((Region)regionObjCollec[0], height, 0.0);
-
-                // append elements into database. 
-                BlockTableRecord blkRecord = trans.GetObject(blk[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                blkRecord.AppendEntity(solid3d);                                              
-
-                trans.AddNewlyCreatedDBObject(solid3d, true);                
-
-                trans.Commit();
+                catch (System.Exception)
+                {
+                    trans.Abort();
+                    throw;
+                }                
             }
         }
 
@@ -537,66 +569,75 @@ namespace Draw_Balloon_net
 
             using (Transaction trans = db.TransactionManager.StartTransaction())
             {
-                BlockTable blkTable = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
-                if (blkTable == null)
+                try
                 {
-                    return;
-                }
-
-                BlockTableRecord blkTableRecord = trans.GetObject(blkTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                if (blkTableRecord == null)
-                {
-                    return;
-                }
-
-                using (Solid3d solid3DBox = new Solid3d())
-                {
-                    // 3D Solid Box.
-                    solid3DBox.CreateBox(5, 7, 10);
-                    solid3DBox.ColorIndex = 7;
-
-                    // Position of the center of solid3DBox at (5, 5, 0)
-                    solid3DBox.TransformBy(Matrix3d.Displacement(new Point3d(5, 5, 0) - Point3d.Origin));
-
-                    //blkTableRecord.AppendEntity(solid3DBox);
-                    //trans.AddNewlyCreatedDBObject(solid3DBox, true);
-
-                    // 3D Solid Cylinder. 
-                    using (Solid3d solid3DCylinder = new Solid3d())
+                    BlockTable blkTable = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                    if (blkTable == null)
                     {
-                        solid3DCylinder.CreateFrustum(20, 5, 5, 5);
-                        solid3DCylinder.ColorIndex = 4;
-
-                        //blkTableRecord.AppendEntity(solid3DCylinder);
-                        //trans.AddNewlyCreatedDBObject(solid3DCylinder, true);
-
-                        // Create 3D solid from the interference of the box and cylinder. 
-                        //Solid3d solid3dCopy = solid3DCylinder.Clone() as Solid3d;
-
-                        //if (solid3dCopy.CheckInterference(solid3DBox) == true)
-                        //{
-                        //    solid3dCopy.BooleanOperation(BooleanOperationType.BoolSubtract, solid3DBox.Clone() as Solid3d);
-                        //    solid3dCopy.ColorIndex = 1;
-                        //}
-
-                        //// add solid3dCopy to the block table record. 
-                        //blkTableRecord.AppendEntity(solid3dCopy);
-                        //trans.AddNewlyCreatedDBObject(solid3dCopy, true);
-
-                        Solid3d solid3dCopyCylinder = solid3DCylinder.Clone() as Solid3d;
-
-                        if (solid3dCopyCylinder.CheckInterference(solid3DBox) == true)
-                        {
-                            solid3dCopyCylinder.BooleanOperation(BooleanOperationType.BoolIntersect, solid3DBox);
-                            solid3dCopyCylinder.ColorIndex = 3;
-                        }
-
-                        blkTableRecord.AppendEntity(solid3dCopyCylinder);
-                        trans.AddNewlyCreatedDBObject(solid3dCopyCylinder, true);
+                        trans.Abort();
+                        return;
                     }
-                }
 
-                trans.Commit();
+                    BlockTableRecord blkTableRecord = trans.GetObject(blkTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                    if (blkTableRecord == null)
+                    {
+                        return;
+                    }
+
+                    using (Solid3d solid3DBox = new Solid3d())
+                    {
+                        // 3D Solid Box.
+                        solid3DBox.CreateBox(5, 7, 10);
+                        solid3DBox.ColorIndex = 7;
+
+                        // Position of the center of solid3DBox at (5, 5, 0)
+                        solid3DBox.TransformBy(Matrix3d.Displacement(new Point3d(5, 5, 0) - Point3d.Origin));
+
+                        //blkTableRecord.AppendEntity(solid3DBox);
+                        //trans.AddNewlyCreatedDBObject(solid3DBox, true);
+
+                        // 3D Solid Cylinder. 
+                        using (Solid3d solid3DCylinder = new Solid3d())
+                        {
+                            solid3DCylinder.CreateFrustum(20, 5, 5, 5);
+                            solid3DCylinder.ColorIndex = 4;
+
+                            //blkTableRecord.AppendEntity(solid3DCylinder);
+                            //trans.AddNewlyCreatedDBObject(solid3DCylinder, true);
+
+                            // Create 3D solid from the interference of the box and cylinder. 
+                            //Solid3d solid3dCopy = solid3DCylinder.Clone() as Solid3d;
+
+                            //if (solid3dCopy.CheckInterference(solid3DBox) == true)
+                            //{
+                            //    solid3dCopy.BooleanOperation(BooleanOperationType.BoolSubtract, solid3DBox.Clone() as Solid3d);
+                            //    solid3dCopy.ColorIndex = 1;
+                            //}
+
+                            //// add solid3dCopy to the block table record. 
+                            //blkTableRecord.AppendEntity(solid3dCopy);
+                            //trans.AddNewlyCreatedDBObject(solid3dCopy, true);
+
+                            Solid3d solid3dCopyCylinder = solid3DCylinder.Clone() as Solid3d;
+
+                            if (solid3dCopyCylinder.CheckInterference(solid3DBox) == true)
+                            {
+                                solid3dCopyCylinder.BooleanOperation(BooleanOperationType.BoolIntersect, solid3DBox);
+                                solid3dCopyCylinder.ColorIndex = 3;
+                            }
+
+                            blkTableRecord.AppendEntity(solid3dCopyCylinder);
+                            trans.AddNewlyCreatedDBObject(solid3dCopyCylinder, true);
+                        }
+                    }
+
+                    trans.Commit();
+                }
+                catch(System.Exception)
+                {
+                    trans.Abort();
+                    throw;
+                }                                
             }
 
         }
